18796d6bc5e4f33d5f4a47dcb389f078c3ef8f4d
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-12-09 21:21:47 +0100

    add docstrings

--- dcachefs/dcachefs.py
+++ dcachefs/dcachefs.py
@@ -17,13 +17,14 @@ DCACHE_FILE_TYPES = {
 
 def _get_details(path, data):
     """
+    Extract details from the metadata returned by the dCache API
 
-    :param path: (string)
-    :param data: (dict)
+    :param path: (str) file or directory path
+    :param data: (dict) metadata as provided by the API
     """
     path = urlpath.URL(path)
 
-    name = data.get('fileName')
+    name = data.get('fileName')  # fileName might be missing
     name = path / name if name is not None else path
     name = name.path
     type = data.get('fileType')
@@ -46,7 +47,12 @@ def _encode(path):
 
 
 class dCacheFileSystem(AbstractFileSystem):
+    """
+    Interface for dCache
 
+    It makes use of the API for all commands but reading/writing, for which the
+    WebDAV door is employed
+    """
     def __init__(
         self,
         api_url=None,
@@ -56,6 +62,15 @@ class dCacheFileSystem(AbstractFileSystem):
         webdav_url=None,
         **storage_options
     ):
+        """
+        :param api_url: (str) URL of the dCache API
+        :param username: (str) to be provided with password
+        :param password: (str) to be provided with username
+        :param token: (str) macaroon for bearer token authentication
+        :param webdav_url: (str) WebDAV door (only used for reading/writing)
+        :param storage_options: (dict) additional arguments passed on to the
+            super class
+        """
         super().__init__(self, **storage_options)
 
         self._api_url = api_url
@@ -85,41 +100,80 @@ class dCacheFileSystem(AbstractFileSystem):
 
     @classmethod
     def _strip_protocol(cls, path):
+        """
+        Turn path from fully-qualified to file-system-specific
+
+        :param path: (str)
+        :return (str)
+        """
         if isinstance(path, list):
             return [cls._strip_protocol(p) for p in path]
         return urlpath.URL(path).path
 
     @staticmethod
     def _get_kwargs_from_urls(path):
+        """
+        Extract kwargs encoded in the path
+
+        :param path: (str)
+        :return (dict)
+        """
         kwargs = {}
         drive = urlpath.URL(path).drive
         if drive:
             kwargs.update(webdav_url=drive)
         return kwargs
 
-    def kw(self, kwargs=None):
-        kw = kwargs or {}
+    def _update_authentication_kwargs(self, kwargs):
+        """
+        Add authentication credentials to kwargs for requests
+
+        :param kwargs: (dict)
+        :return (dict)
+        """
         if self.token is not None:
-            headers = kw.get('headers', {})
+            headers = kwargs.get('headers', {})
             headers.update(Authorization=f'Bearer {self.token}')
-            kw.update(headers=headers)
+            kwargs.update(headers=headers)
         if self.username is not None and self.password is not None:
             auth = (self.username, self.password)
-            kw.update(auth=auth)
-        return kw
+            kwargs.update(auth=auth)
+        return kwargs
 
     def _get_metadata(self, path, children=False, limit=None, **kwargs):
+        """
+        Request file or directory metadata to the API
+
+        :param path: (str)
+        :param children: (bool) if True, return metadata of the children paths
+            as well
+        :param limit: (int) if provided and children is True, set limit to the
+            number of children returned
+        :param kwargs: (dict) optional arguments passed on to requests
+        :return (dict) path metadata
+        """
         path = self._strip_protocol(path)
         url = self.api_url / 'namespace' / _encode(path)
         url = url.with_query(children=children)
-        if limit is not None:
+        if limit is not None and children:
             url = url.add_query(limit=f'{limit}')
 
-        r = url.get(**self.kw(kwargs))
+        r = url.get(**self._update_authentication_kwargs(kwargs.copy()))
         r.raise_for_status()
         return r.json()
 
     def ls(self, path, detail=True, limit=None, **kwargs):
+        """
+        List path content.
+
+        :param path: (str)
+        :param detail: (bool) if True, return a list of dictionaries with the
+            (children) path(s) info. If False, return a list of paths
+        :param limit: (int) set the maximum number of children paths returned
+            to this value
+        :param kwargs: (dict) optional arguments passed on to requests
+        :return list of dictionaries or list of str
+        """
         path = self._strip_protocol(path)
         metadata = self._get_metadata(path, children=True, limit=limit,
                                       **kwargs)
@@ -131,6 +185,13 @@ class dCacheFileSystem(AbstractFileSystem):
             return [d.get('name') for d in details]
 
     def mv(self, path1, path2, **kwargs):
+        """
+        Rename path1 to path2
+
+        :param path1: (str) source path
+        :param path2: (str) destination path
+        :param kwargs: (dict) optional arguments passed on to requests
+        """
 
         path1 = self._strip_protocol(path1)
         url = self.api_url / 'namespace' / _encode(path1)
@@ -138,27 +199,49 @@ class dCacheFileSystem(AbstractFileSystem):
         path2 = self._strip_protocol(path2)
         data = dict(action='mv', destination=path2)
 
-        r = url.post(json=data, **self.kw(kwargs))
+        r = url.post(json=data, **self._update_authentication_kwargs(kwargs))
         r.raise_for_status()
 
     def _rm(self, path):
+        """
+        Remove file or directory (must be empty)
 
+        :param path: (str)
+        """
         path = self._strip_protocol(path)
         url = self.api_url / 'namespace' / _encode(path)
 
-        r = url.delete(**self.kw())
+        r = url.delete(**self._update_authentication_kwargs())
         r.raise_for_status()
 
     def info(self, path, **kwargs):
+        """
+        Give details about a file or a directory
 
+        :param path: (str)
+        :param kwargs: (dict) optional arguments passed on to requests
+        :return (dict)
+        """
         path = self._strip_protocol(path)
         metadata = self._get_metadata(path, **kwargs)
         return _get_details(path, metadata)
 
     def created(self, path):
+        """
+        Date and time in which the path was created
+
+        :param path: (str)
+        :return (datetime.datetime object)
+        """
         return self.info(path).get('created')
 
     def modified(self, path):
+        """
+        Date and time in which the path was last modified
+
+        :param path: (str)
+        :return (datetime.datetime object)
+        """
         return self.info(path).get('modified')
 
     def _open(
@@ -212,7 +295,7 @@ class dCacheFile(AbstractBufferedFile):
         headers = kwargs.pop('headers', {})
         headers.update(Range="bytes=%i-%i" % (start, end - 1))
         kwargs.update(headers=headers)
-        kwargs = self.fs.kw(kwargs)
+        kwargs = self.fs._update_authentication_kwargs(kwargs)
 
         r = url.get(**kwargs)
 
@@ -257,7 +340,7 @@ class dCacheFile(AbstractBufferedFile):
         headers = kwargs.pop('headers', {})
         headers.update(Range="bytes=%i-%i" % (start, end - 1))
         kwargs.update(headers=headers)
-        kwargs = self.fs.kw(kwargs)
+        kwargs = self.fs._update_authentication_kwargs(kwargs)
 
         r = url.put(data=data, **kwargs)
         r.raise_for_status()
