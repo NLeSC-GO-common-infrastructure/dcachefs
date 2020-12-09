e23dcd42c23caf72f1222b0915fc96b360d27cd6
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-12-09 14:23:39 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-# To update the package version number, edit CITATION.cff
-with open('CITATION.cff', 'r') as cff:
-    for line in cff:
-        if 'version:' in line:
-            version = line.replace('version:', '').strip().strip('"')
+version = {}
+# To update the package version number, edit dcachefs/__version__.py
+with open(os.path.join(here, 'dcachefs', '__version__.py')) as f:
+    exec(f.read(), version)
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read().split()
 
 setup(
     name='dcachefs',
@@ -41,7 +42,7 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
-    install_requires=[],  # FIXME: add your package's dependencies to this list
+    install_requires=requirements,
     setup_requires=[
         # dependency for `python setup.py test`
         'pytest-runner',
@@ -54,9 +55,5 @@ setup(
         'pytest',
         'pytest-cov',
         'pycodestyle',
-    ],
-    extras_require={
-        'dev':  ['prospector[with_pyroma]', 'yapf', 'isort'],
-    },
-    data_files=[('citation/dcachefs', ['CITATION.cff'])]
+    ]
 )
