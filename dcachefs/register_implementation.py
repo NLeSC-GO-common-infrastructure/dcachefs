b6e722b3b13ae7daf4fc4bef7ccc6b36e1fa2296
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-12-09 21:29:49 +0100

    add docstring to register_implementation context manager

--- dcachefs/register_implementation.py
+++ dcachefs/register_implementation.py
@@ -6,6 +6,12 @@ from .dcachefs import dCacheFileSystem
 
 @contextlib.contextmanager
 def register_implementation(protocol='https'):
+    """
+    Register dCacheFileSystem as fsspec backend
+
+    :param protocol: (str) URLs with this protocol will be open using
+        dCacheFileSystem from fsspec
+    """
     fsspec.register_implementation(protocol, dCacheFileSystem, clobber=True)
     try:
         yield
