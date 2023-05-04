37ccdf200786d85176aa635d57c43ae409632875
Francesco Nattino <f.nattino@esciencecenter.nl>
2023-05-04 22:37:04 +0200

    refactor register implementation

---
+++ tests/test_init.py
@@ -0,0 +1,8 @@
+import fsspec
+
+from dcachefs import dCacheFileSystem
+
+
+def test_dcachefs_is_registered_as_dcache_protocol():
+    assert fsspec.get_filesystem_class('dcache') == dCacheFileSystem
+    assert isinstance(fsspec.filesystem('dcache'), dCacheFileSystem)
